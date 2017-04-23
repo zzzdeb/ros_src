16b4424e906cb943d96c540788cc63e13345c266
Felix Staudigl <fstaudigl@imr.rwth-aachen.de>
2017-04-23 17:17:25 +0200

    edited

--- fast_laser_segment_2d/src/angle_segment_2d_pub.cpp
+++ fast_laser_segment_2d/src/angle_segment_2d_pub.cpp
@@ -10,7 +10,7 @@ using namespace std;
 
 namespace anglesegment
 {
-
+typedef boost::shared_ptr<vector<int>> ViPtr;
 #define PI 3.14159265358979323846
 
 AngleSegment2dPub::AngleSegment2dPub(ros::NodeHandle *n, int angle_threshold) : nh_(*n), angle_threshold(angle_threshold)
@@ -24,33 +24,33 @@ AngleSegment2dPub::AngleSegment2dPub(ros::NodeHandle *n, int angle_threshold) : 
     ROS_INFO("LaserToCloud constructed");
 }
 
-void AngleSegment2dPub::scan_callback(const sensor_msgs::LaserScan::ConstPtr &scan_msg)
+void AngleSegment2dPub::scan_callback(const LaserScan::ConstPtr &scan_msg)
 {
     scan_msg_ = scan_msg;
     leer_scan->header = scan_msg_->header;
-    boost::shared_ptr<vector<vector<int>>> labels(new vector<vector<int>>);
-    angle_segment_2d(labels);
+    boost::shared_ptr<vector<ViPtr>> labelptr(new vector<ViPtr>);
+    angle_segment_2d(labelptr);
     ///Debug
     // labels->clear();
     // labels.push_back(vector<int>(1,1));
-    for (int i = 0; i < labels->size(); i++)
+    for (int i = 0; i < labelptr->size(); i++)
     {
         LaserScan::Ptr obj = LaserScan::Ptr(new LaserScan());
         obj->header = scan_msg_->header;
-        obj->angle_min = scan_msg_->angle_min;
-        obj->angle_max = scan_msg_->angle_max;
+        obj->angle_min = scan_msg_->angle_min+scan_msg_->angle_increment*(labelptr->at(i)->front());
+        obj->angle_max = scan_msg_->angle_max+scan_msg_->angle_increment*(labelptr->at(i)->back());
         obj->angle_increment = scan_msg_->angle_increment;
         obj->time_increment = scan_msg_->time_increment;
         obj->scan_time = scan_msg_->scan_time;
         obj->range_min = scan_msg_->range_min;
         obj->range_max = scan_msg_->range_max;
-        obj->ranges.resize((*labels)[i].size());
+        obj->ranges.resize(labelptr->at(i)->size());
         for (int j = 0; j < obj->ranges.size(); j++)
-            obj->ranges[j] = scan_msg_->ranges[(*labels)[i][j]];
+            obj->ranges[j] = scan_msg_->ranges[(*labelptr)[i]->at(j)];
         segmented_scans.push_back(obj);
     }
     publish_scans();
-    ROS_INFO("%i segmentation", labels->size());
+    ROS_INFO("%i segmentation", labelptr->size());
     segmented_scans.clear();
 }
 
@@ -61,11 +61,18 @@ void AngleSegment2dPub::publish_scans()
         scan_pubs.resize(segmented_scans.size());
         for (unsigned int i = 0; i < scan_pubs.size(); i++)
         {
-            std::ostringstream out;
-            out << "object " << i + 1;
-            scan_pubs[i] = nh_.advertise<LaserScan>("a" + std::to_string(i), 1);
+            scan_pubs[i] = nh_.advertise<LaserScan>('S' + std::to_string(i), 1);
         }
     } // resizing
+
+    if (segmented_scans.size() < scan_pubs.size() - 5)
+    {
+        scan_pubs.resize(segmented_scans.size());
+        for (unsigned int i = 0; i < scan_pubs.size(); i++)
+        {
+            scan_pubs[i] = nh_.advertise<LaserScan>('S' + std::to_string(i), 1);
+        }
+    }
 
     for (unsigned int i = 0; i < segmented_scans.size(); i++)
     {
@@ -78,40 +85,45 @@ void AngleSegment2dPub::publish_scans()
     }
 }
 
-void AngleSegment2dPub::angle_segment_2d(const boost::shared_ptr<vector<vector<int>>> &output)
+void AngleSegment2dPub::angle_segment_2d(const boost::shared_ptr<vector<ViPtr>> &labelptr)
 {
     //vector<vector<int> > output;
-    vector<int> labels(scan_msg_->ranges.size(), 0);
+    vector<int> labels(scan_msg_->ranges.size());
     int label = 1;
     for (unsigned int i = 0; i < labels.size(); i++) // only one loop because of one laser scan;
     {
         if (labels[i] == 0)
         {
             //ROS_INFO("angle_segment_2d");
-            vector<int> temp = label_component_bfs(i, label, labels);
-            if (temp.size() > 5)
-                output->push_back(temp);
+            ViPtr temp = label_component_bfs(i, label, labels);
+            if (temp->size() > 5)
+                labelptr->push_back(temp);
             label++;
         }
     }
     //return output;
 }
 
-vector<int> AngleSegment2dPub::label_component_bfs(int i, int label, vector<int> &labels)
+ViPtr AngleSegment2dPub::label_component_bfs(int i, int label, vector<int> &labels)
 {
     queue<int> l_queue;
     l_queue.push(i);
-    vector<int> output;
+    ViPtr output(new vector<int>);
 
     while (!l_queue.empty())
     {
         i = l_queue.front();
+        l_queue.pop();
+        if (scan_msg_->ranges[i] > scan_msg_->range_max && scan_msg_->ranges[i] < scan_msg_->range_min)
+        {
+            labels[i] = -1;
+            continue;
+        }
         labels[i] = label;
-        output.push_back(i);
+        output->push_back(i);
         //for neighboars
         if (angle_test(i + 1))
             l_queue.push(i + 1);
-        l_queue.pop();
     }
     return output;
 }
@@ -121,7 +133,7 @@ bool AngleSegment2dPub::angle_test(unsigned int i)
     if (i == scan_msg_->ranges.size())
         return false; // out of range
     float rmax, rmin, alpha;
-    alpha = scan_msg_->angle_increment * 180 / PI;
+    alpha = scan_msg_->angle_increment;
     if (scan_msg_->ranges[i] > scan_msg_->ranges[i - 1])
     {
         rmax = scan_msg_->ranges[i];
