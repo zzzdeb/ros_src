54b64abc60a6f875a68b9586c7b13d1d7016b641
Zolboo Erdenebayar <zolboo.erdenebayar@rwth-aachen.de>
2017-04-24 11:03:15 +0200

    assembler

--- laser_assembler_demo/src/laser_assembler_demo.cpp
+++ laser_assembler_demo/src/laser_assembler_demo.cpp
@@ -10,16 +10,26 @@ int main(int argc, char **argv)
   ros::service::waitForService("assemble_scans");
   ros::ServiceClient client = n.serviceClient<AssembleScans>("assemble_scans");
   AssembleScans srv;
-  srv.request.begin = ros::Time(0,0);
-  srv.request.end   = ros::Time::now();
+  srv.request.begin = ros::Time(0.0);
+  //srv.request.begin = ros::Time::now() - ros::Duration(5);
+  srv.request.end = ros::Time::now();
 
-  ros::Publisher pub = n.advertise<sensor_msgs::PointCloud>("assembled_laser",1);
-  if (client.call(srv)){
-    printf("Got cloud with %u points\n", srv.response.cloud.points.size());
-    pub.publish(srv.response.cloud);
-    ros::spin();
+  ros::Publisher pub = n.advertise<sensor_msgs::PointCloud>("assembled_laser", 1);
+  ros::Rate hz(1);
+  while (ros::ok())
+  {
+    if (client.call(srv))
+    {
+      printf("Got cloud with %u points\n", srv.response.cloud.points.size());
+      //srv.request.begin = ros::Time::now() - ros::Duration(1);
+      srv.request.begin = ros::Time(0.0);
+      srv.request.end = ros::Time::now();
+      pub.publish(srv.response.cloud);
+      ros::spinOnce();
+      hz.sleep();
+    }
+    else
+      printf("Service call failed\n");
   }
-  else
-    printf("Service call failed\n");
   return 0;
 }
